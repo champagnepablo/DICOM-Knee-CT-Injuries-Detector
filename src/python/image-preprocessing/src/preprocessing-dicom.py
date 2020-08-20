5a71aeed6616b83cecb23b5753e8ca11e8dcb480
champagnepablo <pvaleiro96@gmail.com>
2020-08-20 10:27:10 +0200

    Finished femur segmentation

--- src/python/image-preprocessing/src/preprocessing-dicom.py
+++ src/python/image-preprocessing/src/preprocessing-dicom.py
@@ -74,17 +74,16 @@ def thresholdCTImage(img):
 def drawCTContours(originalImage, tresholdedImage):
     contours, _ = cv2.findContours(tresholdedImage, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
     sorted_contours = sorted(contours, key = cv2.contourArea, reverse= True)
-    originalImage = originalImage * 255
     bgrImage = cv2.cvtColor(originalImage, cv2.COLOR_GRAY2BGR)
     cv2.drawContours(bgrImage, contours, -1, (0, 255, 0), 1) 
     return bgrImage, contours
 
-def getLowestPoints(originalImage, tresholdedImage):
+def getLowestPointsFemur(originalImage, tresholdedImage):
     contours, _ = cv2.findContours(tresholdedImage, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
     sorted_contours = sorted(contours, key = cv2.contourArea, reverse= True)
     half_one = sorted_contours[0]
     half_two = sorted_contours[1]
-    originalImage = originalImage * 255
+    originalImage = originalImage.astype(np.uint8)
     bgrImage = cv2.cvtColor(originalImage, cv2.COLOR_GRAY2BGR)
     cv2.drawContours(bgrImage, [half_one], 0, (0,255,0), 1)
     cv2.drawContours(bgrImage, [half_two], 0, (255,0,0), 1)
@@ -97,7 +96,31 @@ def getLowestPoints(originalImage, tresholdedImage):
     cv2.circle(bgrImage, p1, 1, (0, 50, 255), -1)
     cv2.circle(bgrImage, p2, 1, (0, 50, 255), -1)
     cv2.line(bgrImage, (0, point1), (bgrImage.shape[1], point2), (255,80,0), 1)
-    return bgrImage, contours
+    return bgrImage, m
+
+def getTransvesalPointRotula(originalImage, thresholdedImage, femurSlope):
+    contours, _ = cv2.findContours(thresholdedImage, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    sorted_contours = sorted(contours, key = cv2.contourArea, reverse= True)
+    rotula_contour = sorted_contours[1]
+    originalImage = originalImage.astype(np.uint8)
+  #  originalImage = cv2.cvtColor(originalImage, cv2.COLOR_GRAY2BGR)
+    cv2.drawContours(originalImage, [rotula_contour], 0, (255,0,0), 1)
+    top = tuple(rotula_contour[rotula_contour[:, :, 1].argmin()][0]) 
+    bottom = tuple(rotula_contour[rotula_contour[:, :, 1].argmax()][0])
+    cv2.circle(originalImage, top, 1, (0, 50, 255), -1)
+    cv2.circle(originalImage, bottom, 1, (0, 50, 255), -1)
+    m, b = getLineEquation(top, bottom)
+    slope = -1 / m 
+    midpointX = (top[0] + bottom[0]) / 2 
+    midpointY = (top[1] + bottom[1]) / 2
+    b2 = -slope * midpointX + midpointY
+    point1 = (int) (slope * 0 + b2)
+    point2 = (int) (slope * originalImage.shape[1] + b2)
+    cv2.line(originalImage, (0, point1), (originalImage.shape[1], point2), (255,80,0), 1)
+    point3 =  (int) (femurSlope * 0 + b2)
+    point4 = (int) (femurSlope * originalImage.shape[1] + b2)
+    cv2.line(originalImage, (0, point3), (originalImage.shape[1], point4), (255,80,0), 1)
+    return originalImage
 
 
 
@@ -111,11 +134,11 @@ def getContouredCTImage(originalImage):
 '''
 
 
-def cropKneeCT(originalImage):
+def cropFemurCT(originalImage):
     h, w = originalImage.shape
     newHeigth = (int) (np.floor(h / 2))
     newWidth = (int)  (np.floor( w / 2))
-    croppedImage = originalImage[200:375, 100:256]
+    croppedImage = originalImage[160:325, 112:250]
     moments = cv2.moments(croppedImage)
     cX = int(moments["m10"] / moments["m00"])
     cY = int(moments["m01"] / moments["m00"])
@@ -124,15 +147,40 @@ def cropKneeCT(originalImage):
 
 
 
+def cropRotulaCT(originalImage):
+    h, w = originalImage.shape
+    newHeigth = (int) (np.floor(h / 2))
+    newWidth = (int)  (np.floor( w / 2))
+    croppedImage = originalImage[160:325, 112:250]
+    return croppedImage
 
 
 
-ds=pydicom.dcmread('../data/dicom/Knee/vhf.499.dcm')
+
+ds=pydicom.dcmread('../data/dicom/Knee/vhf.421.dcm')
 #plt.imshow(ds.pixel_array, cmap=plt.cm.bone)
 img = transformToHu(ds,ds.pixel_array)
-img = thresholdCTImage(img)
-img2 = img.copy()  
-img3 = cropKneeCT(img)
-img4, _ = getLowestPoints(img3, img3)
-plt.imshow(img4)
+imgNoTh = transformToHu(ds,ds.pixel_array)
+imgNoTh = imgNoTh -  imgNoTh.min()
+num_rows, num_cols = img.shape[:2]
+rotation_matrix = cv2.getRotationMatrix2D((num_cols/2, num_rows/2), -12, 1)
+img_rotation = cv2.warpAffine(img, rotation_matrix, (num_cols, num_rows))
+imgNoTh = img_rotation.copy()
+imgNoTh = imgNoTh - imgNoTh.min()
+imgNoTh = imgNoTh / imgNoTh.max() * 255
+originalImage = cropRotulaCT(imgNoTh)
+
+img_rotation_2 = img_rotation.copy()
+femur_cropped = cropFemurCT(img_rotation_2)
+femur_thresholded = thresholdCTImage(femur_cropped)
+originalImage, slope = getLowestPointsFemur(originalImage, femur_thresholded)
+
+
+rotula_cropped = cropRotulaCT(img_rotation)
+rotula_thresholded = thresholdCTImage(rotula_cropped)
+originalImage = getTransvesalPointRotula(originalImage, rotula_thresholded, slope)
+
+
+
+plt.imshow(img)
 plt.show()
--- src/python/image-preprocessing/src/preprocessing-img.py
+++ src/python/image-preprocessing/src/preprocessing-img.py
@@ -3,42 +3,27 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-
-def colorAdjustment(image):
-    #grayImage = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    print(image)
-    image = cv2.medianBlur(image, 3)
-    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    clahe = cv2.createCLAHE(clipLimit=10.0, tileGridSize=(10, 10))
-    c = clahe.apply(gray)
-    ret,binary = cv2.threshold(c, 0, 255, cv2.THRESH_OTSU | cv2.THRESH_BINARY)
-    return binary
-
-    
-def color2(image):
-    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    image = cv2.equalizeHist(gray)
-    ret,binary = cv2.threshold(image, 0, 255, cv2.THRESH_OTSU | cv2.THRESH_BINARY_INV)
-    grad_x = cv2.Sobel(binary,cv2.CV_64F,1,0,ksize=5)  # x
-    grad_y = cv2.Sobel(binary,cv2.CV_64F,0,1,ksize=5)  # y
-    abs_grad_x = cv2.convertScaleAbs(grad_x)
-    abs_grad_y = cv2.convertScaleAbs(grad_y)
-    grad = cv2.addWeighted(abs_grad_x, 0.5, abs_grad_y, 0.5, 0)
-
-
-    return grad
+def cropRotulaCT(originalImage):
+    croppedImage = originalImage[146:370, 236:450]
+    return croppedImage
 
 
 
 
-def processImage(image):
-    img = cv2.imread(image, cv2.IMREAD_COLOR) #read in grayscale mode
-    print(img)
-    img =  colorAdjustment(img)
-    return img
 
 
-img = processImage('rodilla3.png')
 
-plt.imshow(img, cmap = 'gray')
+
+
+img = cv2.imread('../data/img/tatg-r/rodilla.jpg', cv2.IMREAD_COLOR) #read in grayscale mode
+img = cropRotulaCT(img)
+img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+img = cv2.GaussianBlur(img,(3,3),0)
+sobelx = cv2.Sobel(img,cv2.CV_64F,1,0,ksize=5)  # x
+sobely = cv2.Sobel(img,cv2.CV_64F,0,1,ksize=5)  # y
+abs_grad_x = cv2.convertScaleAbs(sobelx)
+abs_grad_y = cv2.convertScaleAbs(sobely)
+
+grad = cv2.addWeighted(abs_grad_x, 0.5, abs_grad_y, 0.5, 0)
+plt.imshow(abs_grad_x, cmap = 'gray')
 plt.show()
