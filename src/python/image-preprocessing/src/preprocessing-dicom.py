10702a8747e3378d5f2eb44efaa62016b6fb5f36
champagnepablo <pvaleiro96@gmail.com>
2020-11-04 18:08:47 +0100

    Added code to get rotula points

--- src/python/image-preprocessing/src/preprocessing-dicom.py
+++ src/python/image-preprocessing/src/preprocessing-dicom.py
@@ -225,11 +225,11 @@ def rotateFemur(img):
 # it positive
     else:
         angle = -angle  
-
+    print(angle)
 # rotate the image to deskew it
     (h, w) = img.shape[:2]
     center = (w // 2, h // 2)
-    M = cv2.getRotationMatrix2D(center, -angle, 1.0)
+    M = cv2.getRotationMatrix2D(center, angle, 1.0)
     rotated = cv2.warpAffine(img, M, (w, h),
     flags=cv2.INTER_CUBIC, borderMode=cv2.BORDER_REPLICATE) 
   #  cv2.drawContours(img, [biggest_contour], 0, (0,255,0), 3)
@@ -269,18 +269,60 @@ def getPointsFemur(img, angle):
     cv2.circle(rotated, ( tf2[1][0][0], tf2[1][0][1]), radius=0, color=(0, 0, 255), thickness=-1)
     return rotated, (tf2[0][0], tf2[1][0])
 
+
+def rotate_rotula(img):
+    contours, _ = cv2.findContours(img, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
+    sorted_contours = sorted(contours, key = cv2.contourArea, reverse= True)
+    femur = sorted_contours[0]
+    extTop = tuple(femur[femur[:, :, 1].argmin()][0])
+    for i in  range(img.shape[0]-1):
+        for j in range(img.shape[1]-1):
+            if extTop[1] < i :
+                img[i][j] = 0   #remove anything
+    rotula_contour = sorted_contours[1]
+    rect = cv2.minAreaRect(rotula_contour)
+    angle = rect[2]
+
+    if angle < -45:
+        angle = (90 + angle)
+
+# otherwise, just take the inverse of the angle to make
+# it positive
+    else:
+        angle = -angle  
+    print(angle)
+# rotate the image to deskew it
+    (h, w) = img.shape[:2]
+    center = (w // 2, h // 2)
+    M = cv2.getRotationMatrix2D(center, angle, 1.0)
+    rotated = cv2.warpAffine(img, M, (w, h),
+    flags=cv2.INTER_CUBIC, borderMode=cv2.BORDER_REPLICATE) 
+    contours, _ = cv2.findContours(rotated, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
+    sorted_contours = sorted(contours, key = cv2.contourArea, reverse= True)
+    c = sorted_contours[0]
+    extLeft = tuple(c[c[:, :, 0].argmin()][0])
+    extRight = tuple(c[c[:, :, 0].argmax()][0])
+    transform_points = np.array( [ [ [ extLeft[0], extLeft[1] ] ],  [ [ extRight[0], extRight[1] ] ]  ])
+    M = cv2.getRotationMatrix2D(center, -angle, 1.0)
+    rotated = cv2.warpAffine(img, M, (w, h),
+    flags=cv2.INTER_CUBIC, borderMode=cv2.BORDER_REPLICATE) 
+    tf2 = cv2.transform(transform_points, M)
+
+    return rotated, (tf2[0][0], tf2[1][0])
+
+
+
+
+
 #ds=pydicom.dcmread('/home/pablo/Documentos/TFG/src/python/image-preprocessing/data/dicom/serie/4859838 serie completa.Seq4.Ser4.Img100.dcm')
-ds=pydicom.dcmread('/home/pablo/Documentos/TFG/src/python/image-preprocessing/data/dicom/prueba.dcm')
+ds=pydicom.dcmread('/home/pablo/Documentos/TFG/src/python/image-preprocessing/data/dicom/prueba2.dcm')
 #plt.imshow(ds.pixel_array, cmap=plt.cm.bone)
 img = transformToHu(ds,ds.pixel_array)
-print(ds.WindowWidth)
-img_norm = normalizeImage(img, ds.WindowCenter[1], ds.WindowWidth[0])
-th_img = thresholdCTImage(img, (ds.WindowCenter[1]  + ds.WindowCenter[0]) / 2, ds.WindowWidth[0] )
-kernel = np.ones((2,2))
-closing = cv2.morphologyEx(th_img, cv2.MORPH_CLOSE, kernel)
-roi_img = getROI(closing)
+th_img = thresholdCTImage(img,ds.WindowCenter[0] , ds.WindowWidth[0])
+
+roi_img = getROI(th_img)
 rotated_img, angle = rotateFemur(roi_img)
-cropped_img, (extBot, extBot2) = getPointsFemur(rotated_img, angle)
+cropped_img, (extBot, extBot2) = getPointsFemur(rotated_img, -angle)
 img_2d = ds.pixel_array.astype(float)
 
 ## Step 2. Rescaling grey scale between 0-255
@@ -288,16 +330,24 @@ img_2d_scaled = (np.maximum(img_2d,0) / img_2d.max()) * 255.0
 
 ## Step 3. Convert to uint
 img_2d_scaled = np.uint8(img_2d_scaled)
-
+print(ds.WindowWidth)
 img2 = cv2.cvtColor(img_2d_scaled, cv2.COLOR_GRAY2BGR)
 cv2.circle(img2, (extBot[0], extBot[1]), radius=0, color=(0, 0, 255), thickness=-1)
 cv2.circle(img2, (extBot2[0], extBot2[1]), radius=0, color=(0, 0, 255), thickness=-1)
+
+rotula_img , (extLeft, extRight) = rotate_rotula(roi_img)
+
+cv2.circle(img2, (extLeft[0], extLeft[1]), radius=0, color=(0, 0, 255), thickness=-1)
+cv2.circle(img2, (extRight[0], extRight[1]), radius=0, color=(0, 0, 255), thickness=-1)
+
+
+
+print(extRight)
 
 '''
 num_rows, num_cols = img.shape[:2]
 rotation_matrix = cv2.getRotationMatrix2D((num_cols/2, num_rows/2), -12, 1)
 img_rotation = cv2.warpAffine(img, rotation_matrix, (num_cols, num_rows))
 '''
-print(angle)
-plt.imshow(img_norm)
+plt.imshow(img2)
 plt.show()
